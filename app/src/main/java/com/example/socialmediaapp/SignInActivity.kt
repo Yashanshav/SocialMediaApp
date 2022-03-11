7381e6bca914c09cd3264a58f366cf5cfc929555
yashanshav <yashanshavvrajpurohit@gmail.com>
2022-03-11 08:42:07 +0530

    Basic sign in with Google

---
+++ app/src/main/java/com/example/socialmediaapp/SignInActivity.kt
@@ -0,0 +1,126 @@
+package com.example.socialmediaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN: Int = 123
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val TAG = "SignInActivity Tag"
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        // Redundant error that path is already in project -> app -> build -> generated -> res -> google-services -> values -> values.xml
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+
+
+        val signInButton = findViewById<SignInButton>(R.id.signInButton)
+        signInButton.setOnClickListener {
+            signIn()
+        }
+
+
+    }
+
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+
+        // this activity help in getting all accounts to select from
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            // created by us to handle sign in
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            // Google Sign In was successful, authenticate with Firebase
+            val account = completedTask.getResult(ApiException::class.java)!!
+            Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException) {
+            // Google Sign In failed, update UI appropriately
+            Log.w(TAG, "Google sign in failed", e)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        val signInButton = findViewById<SignInButton>(R.id.signInButton)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        // changing UI as user should you know he/she has to wait until authenciation is completed.
+        signInButton.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+
+        // using couroutines to authenciate as background task on IO thread.
+        GlobalScope.launch(Dispatchers.IO) {
+            val auth = auth.signInWithCredential(credential).await()
+            val firebaseUser = auth.user
+
+            // but UI component has to be used in UI thread so going back to Main thread.
+            withContext(Dispatchers.Main) {
+                updateUI(firebaseUser)
+            }
+
+        }
+    }
+
+    // Updating UI after authenciation
+    private fun updateUI(firebaseUser: FirebaseUser?) {
+        if(firebaseUser != null) {
+            val mainActivityIntent = Intent(this, MainActivity :: class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        }
+        else {
+            val signInButton = findViewById<SignInButton>(R.id.signInButton)
+            val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+            signInButton.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+        }
+    }
+
+}
